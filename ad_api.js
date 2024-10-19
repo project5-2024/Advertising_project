0627259e754af2e34883fa922e0a9b6ad55dceee
igorkapusniak0 <20102236@mail.wit.ie>
2024-10-19 01:09:57 +0100

    set up register and login

--- ad_api.js
+++ ad_api.js
@@ -13,17 +13,24 @@ app.get("/", (req, res) => {
   res.send("Working");
 });
 
-app.get("/register/username/:username/password/:password", (req,res) => {
-  res.send(req.params);
-  
-  db_insert_user_login(req.params.username, req.params.password, "idk some data")
+app.get("/register/username/:username/password/:password", async (req,res) => {
+  if (!check_username_taken){
+    db_insert_user_login(req.params.username, req.params.password, "idk some data");
+    res.send(req.params);
+  }else {
+    res.send("Username taken");
+  }
 })
 
-app.get("authenticate/username/:username/password/:password", (req,res) => {
-  res.send(req.params);
-  
-  db_insert_user_login(req.params.username, req.params.password, "idk some data")
-})
+app.get("/login/username/:username/password/:password", async (req,res) => {  
+  const userlogin = await authenticate_user(req.params.username, req.params.password);
+  console.log(userlogin);
+  console.log(req.params);
+  if(JSON.stringify(req.params) === JSON.stringify(userlogin)){
+    console.log("Working")
+    res.send(req.params);
+  }
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}!`);
@@ -66,20 +73,49 @@ async function db_insert_ad(image, ad_data){
   }
 }
 
+async function check_username_taken(name){
+  try{
+    const database = client.db(database_name);
+    const users = database.collection(users_collection);
+
+    const query = {
+      username: name,
+    };
+
+    const result = await users.findOne(query);
+
+    if (result == null){
+      return false;
+    }
+    else {
+      return true;
+    } 
+  } finally {
+    console.log("check if user name is taken")
+  }
+}
+
 async function authenticate_user(name, hash){
   try{
     const database = client.db(database_name);
     const users = database.collection(users_collection);
 
-    const doc = {
+    const query = {
       username: name,
       password: hash,
     };
-    const result = await users.find(doc);
+
+    const options = {
+      projection: {
+        _id: 0,
+        ad_preferences: 0,
+      },
+    };
+    
+    const result = await users.findOne(query,options);
+    return result;
   } finally {
-    console.log("found user to DB")
+    console.log("authenticate_user")
   }
 }
-
-
 
